288625c0b94eb6468dd903dc558be78586e95299
evont <evontgoh@foxmail.com>
2017-05-11 23:30:07 +0800

    README changed

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -17,7 +17,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet weak var StopRecording: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup aft  er loading the view, typically from a nib.
         StopRecording.isEnabled = false
     }
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PitchPerfect
 
-![Recording](./imgs/QQ20170511-231543@2x.png)
+iOS 变声器，一共有六种模式，分别为： 慢速播放、 快速播放、 高音调模式、低音调模式、 回音模式、混响模式；
+
+使用了[AVFoundation 框架](https://developer.apple.com/reference/avfoundation) 进行开发
+
+![Recording](./imgs/QQ20170511-231543@2x.png "录音界面")
+![Recorded](./imgs/QQ20170511-231558@2x.png "播放界面")
